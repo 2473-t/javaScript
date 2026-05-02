da849df2c86c4d4043948beb986ac668f627e513
ttesp <56379964+2473-t@users.noreply.github.com>
2026-05-02 14:26:59 +0800

    Update cf-ip-optimizer.js
    fix bug

--- cf-ip-optimizer.js
+++ cf-ip-optimizer.js
@@ -6,7 +6,7 @@
  *
  * ========== 部署方式 ==========
  * [task_local]
- * cron 13 */4 * * * script-path=https://YOUR_HOST/cf-ip-optimizer.js#worker_host=mydomain&max_latency=300&dl_count=15, tag=CF优选, enabled=true
+ * cron 13 *\/4 * * * script-path=https://YOUR_HOST/cf-ip-optimizer.js#worker_host=mydomain&max_latency=300&dl_count=15, tag=CF优选, enabled=true
  *
  * ========== 参数说明 (URL hash) ==========
  * worker_host     - 必填, CF Worker 域名 (用于下载测速)
